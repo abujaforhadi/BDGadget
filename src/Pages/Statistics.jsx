552683c5275cb59d49bce6ea608091294603ee80
abujaforhadi <abujaforhadi@gmail.com>
2024-11-06 14:56:21 +0600

    title

--- src/Pages/Statistics.jsx
+++ src/Pages/Statistics.jsx
@@ -12,7 +12,7 @@ function Statistics() {
     <div>
       <div className="bg-customPurple py-5">
         <Heading
-          title="Statistics | BDGadget"
+          title="Statistics"
           subTitle="Explore the latest gadgets that will take your experience to the next level. From smart devices to the coolest accessories, we have it all!"
         />
       </div>
